50c0409884686a7f3d3b124acd8cb54f8673a174
nouhi <wolfgraphics.business@gmail.com>
2023-08-01 22:12:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function checkStatusGame() {
             const endTime = new Date().getTime();
             const responseTime = endTime - startTime;
 
-            if (xhr.status === 200) {
+            if (xhr.status === 403) {
                 if (responseTime >= 0 && responseTime < 60) {
                     statusElement.textContent = "Operational";
                     statusElement.classList.remove("degraded", "outage");
@@ -92,7 +92,7 @@ function checkStatusIcon() {
             const endTime = new Date().getTime();
             const responseTime = endTime - startTime;
 
-            if (xhr.status === 200) {
+            if (xhr.status === 403) {
                 if (responseTime >= 0 && responseTime < 60) {
                     statusElement.textContent = "Operational";
                     statusElement.classList.remove("degraded", "outage");
